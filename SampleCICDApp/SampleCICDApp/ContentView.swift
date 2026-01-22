5df5bbc09401dc6a882601592dc323563337c428
Shamal Nikam <shamnik2002@gmail.com>
2026-01-21 23:35:08 -0500

    Shamal/project set up (#2)
    * Project set up
    
    * scheme

---
+++ SampleCICDApp/SampleCICDApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SampleCICDApp
+//
+//  Created by Shamal nikam on 1/21/26.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleCICDApp/SampleCICDApp/SampleCICDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCICDAppApp.swift
+//  SampleCICDApp
+//
+//  Created by Shamal nikam on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleCICDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
